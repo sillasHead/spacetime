a8e0fef234a240eeaf56a138da062ee629de7d66
sillasHead <sillas.ap16@gmail.com>
2023-05-18 23:09:22 -0300

    building the homepage and tweaking the settings

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,83 @@
+import { User } from 'lucide-react'
+
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      {/* left */}
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-24 py-16 ">
+        {/* blur */}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+
+        {/* stripes */}
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+        {/* sign in */}
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+
+          <p className="max-w-[180px] text-sm leading-snug">
+            <span className="underline">Create your account</span> and save your
+            memories!
+          </p>
+        </a>
+
+        {/* hero */}
+        <div className="space-y-5">
+          {/* space-y: is similar to gap in flexbox */}
+          <Image src={nlwLogo} alt="NLW Spacetime" />
+
+          <div className="max-w-sm space-y-1">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">
+              Your time capsule
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Collect memorable moments of your jorney and share them with the
+              world
+            </p>
+          </div>
+
+          <a
+            href=""
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+          >
+            CREATE MEMORY
+          </a>
+        </div>
+
+        {/* copyright */}
+        <div className="text-sm leading-relaxed text-gray-200">
+          Learning the best and most modern web development with NLW{' '}
+          <a
+            href="https://rocketseat.com.br"
+            className="underline hover:text-gray-100"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Rocketseat
+          </a>
+        </div>
+      </div>
+
+      {/* right */}
+      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            You have not registered any memories yet, start{' '}
+            <a href="" className="underline hover:text-gray-50">
+              creating now
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
