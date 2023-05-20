0fe754d956eb97d2592c092ef77d3cc4ad9ab87f
sillasHead <sillas.ap16@gmail.com>
2023-05-19 22:05:32 -0300

    adding fastify-static features, turning upload folder public

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,14 @@
 import 'dotenv/config'
 
-import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import multipart from '@fastify/multipart'
-import { memoriesRoutes } from './routes/memories'
+import fastifyStatic from '@fastify/static'
+import fastify from 'fastify'
 import { authRoutes } from './routes/auth'
+import { memoriesRoutes } from './routes/memories'
 import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 
@@ -24,6 +26,13 @@ app.register(jwt, {
 
 // https://github.com/fastify/fastify-multipart
 app.register(multipart)
+
+// https://github.com/fastify/fastify-static
+// turns the folder '/upload' public and a route that can be accessed by the browser
+app.register(fastifyStatic, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(authRoutes)
 app.register(memoriesRoutes)
